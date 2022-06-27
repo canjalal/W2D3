6c2b941fd709733ace817186fb32b613c739e6b4
canjalal <vincent.shuali@gmail.com>
2022-06-26 22:06:52 -0700

    Mastermind project update
    Finished num_near_matches method and most of mastermind.rb

--- vincent_shuali_mastermind_project/lib/code.rb
+++ vincent_shuali_mastermind_project/lib/code.rb
@@ -38,15 +38,31 @@ class Code
     @pegs.length
   end
 
-  def num_exact_matches(code)
-    @pegs.select.with_index {|peg, i| peg == code.pegs[i]}.length
+  def num_exact_matches(codeinst)
+    @pegs.select.with_index {|peg, i| peg == codeinst.pegs[i]}.length
   end
   
-  def num_near_matches(code)
+  def num_near_matches(codeinst)
   
   
-    code.pegs.count {|peg| @pegs.include?(peg)} - num_exact_matches(code)
+    temppegs = @pegs.map.with_index {|peg, i| codeinst.pegs[i] == peg ? nil : peg}.compact
+    tempguess = codeinst.pegs.map.with_index {|peg, i| @pegs[i] == peg ? nil : peg}.compact
+    
+    count = 0
+    tempguess.each do |ele|
+      if(temppegs.index(ele))
+        temppegs.delete_at(temppegs.index(ele))
+        count += 1
+      end
+    end
 
+    count
+
+  end
+
+  def ==(codeinst)
+    return false if @pegs.length != codeinst.pegs.length
+    @pegs == codeinst.pegs
   end
 
 end
--- vincent_shuali_mastermind_project/lib/mastermind.rb
+++ vincent_shuali_mastermind_project/lib/mastermind.rb
@@ -1,5 +1,12 @@
 require_relative "code"
 
 class Mastermind
+    def initialize(length)
+        @secret_code = Code.random(length)
+    end
 
+    def print_matches(codeinst)
+        puts "You have #{@secret_code.num_exact_matches(codeinst)} exact matches"
+        puts "You have #{@secret_code.num_near_matches(codeinst)} near matches"
+    end
 end
