506e80ff807dec72b5b4393f3948b0359818448d
canjalal <vincent.shuali@gmail.com>
2022-06-26 23:26:54 -0700

    Finish Mastermind vincent_shuali_mastermind_project
    It's finally done

--- vincent_shuali_mastermind_project/lib/mastermind.rb
+++ vincent_shuali_mastermind_project/lib/mastermind.rb
@@ -9,4 +9,14 @@ class Mastermind
         puts "You have #{@secret_code.num_exact_matches(codeinst)} exact matches"
         puts "You have #{@secret_code.num_near_matches(codeinst)} near matches"
     end
+
+    def ask_user_for_guess
+        puts "Enter a code"
+        userinput = gets.chomp
+        usercode = Code.from_string(userinput)
+        self.print_matches(usercode)
+        usercode == @secret_code
+
+
+    end
 end
